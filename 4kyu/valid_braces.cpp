f7452245c1aedfff3e8b6f684fb8cea41cf63d0e
Krzysztof Gorecki <kg@b-intohimo.com>
2017-03-11 19:54:00 +0200

    added valid braces

---
+++ 4kyu/valid_braces.cpp
@@ -0,0 +1,35 @@
+// Valid Braces 4th kyu kata
+
+bool validPair(char a, char b)
+{
+  if (a == '[' && b == ']')
+    return true;
+  if (a == '(' && b == ')')
+    return true;
+  if (a == '{' && b == '}')
+    return true;
+  return false;
+}
+
+bool valid_braces(std::string braces) 
+{
+  std::string::iterator bi = braces.begin();
+  std::string::iterator bi_end = braces.end();
+  std::string openBraces("");
+  
+  for (; bi != bi_end; bi++)
+  {
+    if (*bi == '[' || *bi == '(' || *bi == '{')
+      openBraces.push_back(*bi);
+    else if (*bi == ']' || *bi == ')' || *bi == '}')
+    {
+      if (!validPair(*(openBraces.end() - 1), *bi))
+        return false;
+      openBraces.pop_back();
+    }
+  }
+  if (openBraces.length())
+    return false;
+  
+  return true;
+}
