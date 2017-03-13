e7129fef2d0e43039b5c2a61e19a5345dd435cd6
Krzysztof Gorecki <kg@b-intohimo.com>
2017-03-13 09:57:47 +0200

    updated valid braces

--- 4kyu/valid_braces.cpp
+++ 4kyu/valid_braces.cpp
@@ -1,21 +1,10 @@
 // Valid Braces 4th kyu kata
-
-bool validPair(char a, char b)
-{
-  if (a == '[' && b == ']')
-    return true;
-  if (a == '(' && b == ')')
-    return true;
-  if (a == '{' && b == '}')
-    return true;
-  return false;
-}
-
 bool valid_braces(std::string braces) 
 {
   std::string::iterator bi = braces.begin();
   std::string::iterator bi_end = braces.end();
   std::string openBraces("");
+  std::map<char, char> pairs {{']','['}, {')','('}, {'}','{'}};
   
   for (; bi != bi_end; bi++)
   {
@@ -23,7 +12,7 @@ bool valid_braces(std::string braces)
       openBraces.push_back(*bi);
     else if (*bi == ']' || *bi == ')' || *bi == '}')
     {
-      if (!validPair(*(openBraces.end() - 1), *bi))
+      if (*(openBraces.end() - 1) != pairs[*bi])
         return false;
       openBraces.pop_back();
     }
