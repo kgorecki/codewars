c3dfc9fca6ad9ecee6d78eb48cc62fc339221f98
Krzysztof Gorecki <kg@b-intohimo.com>
2017-03-19 21:54:19 +0200

    another hard time bomb

--- 4kyu/hard_time_bomb.js
+++ 4kyu/hard_time_bomb.js
@@ -8,3 +8,12 @@ for (var i = 0; i < vars.length; i++)
 
 Bomb.CutTheWire(global[wireCode]);
 
+
+// another approach
+var wireCode;
+var gkeys = Object.keys(global);
+for (var i = 0; i < gkeys.length; i++)
+  if (gkeys[i].indexOf('boom') != -1)
+    wireCode = global[gkeys[i]];
+Bomb.CutTheWire(wireCode);
+
