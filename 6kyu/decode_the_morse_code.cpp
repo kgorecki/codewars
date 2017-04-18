cd7f4136cada8e22a460e1733dd693c9dbd43443
Krzysztof Gorecki <kg@b-intohimo.com>
2017-04-18 20:11:39 +0300

    added decode the morse code

---
+++ 6kyu/decode_the_morse_code.cpp
@@ -0,0 +1,18 @@
+// Decode the Morse code 6th kyu kata
+
+std::string decodeMorse(std::string morseCode) {
+    std::string decoded;
+    std::istringstream iss(morseCode);
+    std::string tempStr;
+    
+    while (std::getline(iss, tempStr, ' '))
+    {
+      if ("" != tempStr)
+        decoded += MORSE_CODE[tempStr];
+      else
+        if (decoded.back() != ' ' && decoded.length() )
+          decoded += " ";
+    }
+    return decoded;
+}
+
